9c40da790c1bbd313013e391b7ea15ac9fdbe8f8
Truptimayee Barik <truptimayee231@gmail.com>
2021-08-16 08:19:53 +0530

    New Project added

---
+++ ReplaceSpaceWithUnderscore.java
@@ -0,0 +1,13 @@
+package Practice3;
+
+public class ReplaceSpaceWithUnderscore {
+
+	public static void main(String[] args) {
+		String name ="Truptimayee Barik";
+		System.out.println(name);
+		String replaceSpace=name.replace(" ", "_");
+		System.out.println(replaceSpace);
+
+	}
+
+}
